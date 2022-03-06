46c3f9b86b25acf2787055cb6c8c1b14b8094ee9
milad.hasani <milad.link.007@gmail.com>
2022-03-06 14:14:31 +0330

    edit structure

--- darknet.py
+++ darknet.py
@@ -3,81 +3,88 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-def load_conv_bn (buf, start, conv_model, bn_model):
+def load_conv_bn(buf, start, conv_model, bn_model):
     """" load weights on conv & bn layers """
-    num_w = conv_model.weight.numel ()
-    num_b = bn_model.bias.numel ()
-    bn_model.bias.data.copy_ (torch.from_numpy (buf [start: start + num_b])); start = start + num_b
-    bn_model.weight.data.copy_ (torch.from_numpy (buf [start: start + num_b])); start = start + num_b
-    bn_model.running_mean.copy_ (torch.from_numpy (buf [start: start + num_b])); start = start + num_b
-    bn_model.running_var.copy_ (torch.from_numpy (buf [start: start + num_b])); start = start + num_b
-    conv_model.weight.data.copy_ (torch.from_numpy (buf [start: start + num_w]).reshape_as (conv_model.weight)); start = start + num_w
+    num_w = conv_model.weight.numel()
+    num_b = bn_model.bias.numel()
+    bn_model.bias.data.copy_(torch.from_numpy(buf[start: start + num_b])); start = start + num_b
+    bn_model.weight.data.copy_(torch.from_numpy(buf[start: start + num_b])); start = start + num_b
+    bn_model.running_mean.copy_(torch.from_numpy(buf[start: start + num_b])); start = start + num_b
+    bn_model.running_var.copy_(torch.from_numpy(buf[start: start + num_b])); start = start + num_b
+    conv_model.weight.data.copy_(torch.from_numpy(buf[start: start + num_w]).reshape_as(conv_model.weight)); start = start + num_w
     return start
 
 
-def load_conv (buf, start, conv_model):
+def load_conv(buf, start, conv_model):
     """ load weights on conv layer """
-    num_w = conv_model.weight.numel ()
-    num_b = conv_model.bias.numel ()
-    conv_model.bias.data.copy_ (torch.from_numpy (buf [start: start + num_b])); start = start + num_b
-    conv_model.weight.data.copy_ (torch.from_numpy (buf [start: start + num_w]).reshape_as (conv_model.weight)); start = start + num_w
+    num_w = conv_model.weight.numel()
+    num_b = conv_model.bias.numel()
+    conv_model.bias.data.copy_(torch.from_numpy(buf[start: start + num_b])); start = start + num_b
+    conv_model.weight.data.copy_(torch.from_numpy(buf[start: start + num_w]).reshape_as(conv_model.weight)); start = start + num_w
 
 
-class Conv2D (nn.Module):
+class Conv2D(nn.Module):
     """ Conv2D + BatchNormalization + LeakyReLU + MaxPool2d """
-    def __init__ (self, in_channels, out_channels, kernel_size, stride = 1, padding = True, activation = True, pooling = False):
-        super (Conv2D, self).__init__ ()
+    def __init__(self,
+    in_channels,
+    out_channels,
+    kernel_size,
+    stride=1,
+    padding=True,
+    activation=True,
+    pooling=False):
+        super(Conv2D, self).__init__()
         padding = (kernel_size - 1) // 2 if padding else 0
-        self.conv = nn.Conv2d (in_channels, out_channels, kernel_size, stride, padding, bias = False)
-        self.bn   = nn.BatchNorm2d (out_channels)
-        self.relu = nn.LeakyReLU (0.1, inplace = True) if activation else lambda x : x
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
+        self.bn   = nn.BatchNorm2d(out_channels)
+        self.relu = nn.LeakyReLU(0.1, inplace=True) if activation else lambda x : x
         self.pooling = pooling
  
-    def forward (self, x):
-        return F.max_pool2d (self.relu (self.bn (self.conv (x))), 2, 2) if self.pooling else self.relu (self.bn (self.conv (x)))
+    def forward(self, x):
+        return F.max_pool2d(self.relu(self.bn(self.conv(x))), 2, 2) if self.pooling else self.relu(self.bn(self.conv(x)))
 
 
-def Reorg (x, s = 2):
-    B, C, H, W = x.size ()
+def Reorg(x, s=2):
+    B, C, H, W = x.size()
     h, w = H // s, W // s
-    x = x.view (B, C, h, s, w, s).transpose (3, 4).contiguous ()
-    x = x.view (B, C, h * w, s * s).transpose (2, 3).contiguous ()
-    x = x.view (B, C, s * s, h, w).transpose (1, 2).contiguous ()
-    return x.view (B, s * s * C, h, w)
+    x = x.view(B, C, h, s, w, s).transpose(3, 4).contiguous()
+    x = x.view(B, C, h * w, s * s).transpose(2, 3).contiguous()
+    x = x.view(B, C, s * s, h, w).transpose(1, 2).contiguous()
+    return x.view(B, s * s * C, h, w)
 
 
-class Darknet (nn.Module):
-    def __init__ (self):
-        super (Darknet, self).__init__ ()
+class Darknet(nn.Module):
+    def __init__(self):
+        super(Darknet, self).__init__()
       
-        self.main1 = nn. Sequential (     #input = (3, 608, 608)
-            Conv2D (3, 32, 3, pooling = True),
+        self.main1 = nn. Sequential(     #input = (3, 608, 608)
+            Conv2D(3, 32, 3, pooling=True),
  
-            Conv2D (32, 64, 3, pooling = True),
+            Conv2D(32, 64, 3, pooling=True),
  
-            Conv2D (64, 128, 3),
-            Conv2D (128, 64, 1),
-            Conv2D (64, 128, 3, pooling = True),
+            Conv2D(64, 128, 3),
+            Conv2D(128, 64, 1),
+            Conv2D(64, 128, 3, pooling=True),
  
-            Conv2D (128, 256, 3),
-            Conv2D (256, 128, 1),
-            Conv2D (128, 256, 3, pooling = True),
+            Conv2D(128, 256, 3),
+            Conv2D(256, 128, 1),
+            Conv2D(128, 256, 3, pooling=True),
  
-            Conv2D (256, 512, 3),
-            Conv2D (512, 256, 1),
-            Conv2D (256, 512, 3),
-            Conv2D (512, 256, 1),
-            Conv2D (256, 512, 3))
+            Conv2D(256, 512, 3),
+            Conv2D(512, 256, 1),
+            Conv2D(256, 512, 3),
+            Conv2D(512, 256, 1),
+            Conv2D(256, 512, 3))
  
-        self.main2 = nn.Sequential (
-            nn.MaxPool2d (2, stride = 2),
-            Conv2D (512, 1024, 3),
-            Conv2D (1024, 512, 1),
-            Conv2D (512, 1024, 3),
-            Conv2D (1024, 512, 1),
-            Conv2D (512, 1024, 3))
+        self.main2 = nn.Sequential(
+            nn.MaxPool2d(2, stride=2),
+            Conv2D(512, 1024, 3),
+            Conv2D(1024, 512, 1),
+            Conv2D(512, 1024, 3),
+            Conv2D(1024, 512, 1),
+            Conv2D(512, 1024, 3))
  
-    def forward (self, x):
-        x1 = self.main1 (x)
-        x2 = self.main2 (x1)
+    def forward(self, x):
+        x1 = self.main1(x)
+        x2 = self.main2(x1)
         return x2, x1
--- model.py
+++ model.py
@@ -8,59 +8,60 @@ from darknet import *
 
 
 
-class YoloV2Net (nn.Module):
-    def __init__ (self, num_anchors = 5, num_classes = 80):
-        super (YoloV2Net, self).__init__ ()
+class YoloV2Net(nn.Module):
+    def __init__(self, num_anchors=5, num_classes=80):
+        super(YoloV2Net, self).__init__()
         self.num_classes = num_classes
-        self.anchors = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88882, 3.52778, 9.77052, 9.16888]
+        self.anchors = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843,
+                        5.47434, 7.88882, 3.52778, 9.77052, 9.16888]
         self.num_anchors = num_anchors
-        self.darknet = Darknet ()
+        self.darknet = Darknet()
      
-        self.conv1 = nn.Sequential (
-             Conv2D (1024, 1024, 3),
-             Conv2D (1024, 1024, 3))
+        self.conv1 = nn.Sequential(
+             Conv2D(1024, 1024, 3),
+             Conv2D(1024, 1024, 3))
      
-        self.conv2 = nn.Sequential (
-             Conv2D (512, 64, 1))
+        self.conv2 = nn.Sequential(
+             Conv2D(512, 64, 1))
      
-        self.conv  = nn.Sequential (
-             Conv2D (1280, 1024, 3),
-             nn.Conv2d (1024, self.num_anchors * (self.num_classes + 5), 1))
+        self.conv  = nn.Sequential(
+             Conv2D(1280, 1024, 3),
+             nn.Conv2d(1024, self.num_anchors * (self.num_classes + 5), 1))
      
-    def forward (self, x):
-        x1, x2 = self.darknet (x)
-        x1 = self.conv1 (x1)
-        x2 = self.conv2 (x2)
-        x2 = Reorg (x2)
-        x = torch.cat ([x2, x1], 1)
-        x = self.conv (x)
+    def forward(self, x):
+        x1, x2 = self.darknet(x)
+        x1 = self.conv1(x1)
+        x2 = self.conv2(x2)
+        x2 = Reorg(x2)
+        x = torch.cat([x2, x1], 1)
+        x = self.conv(x)
         return x
 
 
-def load_weights (model, wt_file):
+def load_weights(model, wt_file):
     """ load weights from .weights file """
-    buf = np.fromfile (wt_file, dtype = np.float32)
+    buf = np.fromfile(wt_file, dtype=np.float32)
     start = 4   
-    for i in range (13):
-        bn   = model.darknet.main1 [i].bn
-        conv = model.darknet.main1 [i].conv
-        start = load_conv_bn (buf, start, conv, bn)
-    for i in range (1, 6):
-        bn   = model.darknet.main2 [i].bn
-        conv = model.darknet.main2 [i].conv
-        start = load_conv_bn (buf, start, conv, bn)
-    for i in range (2):
-        start = load_conv_bn (buf, start, model.conv1 [i].conv, model.conv1 [i].bn)
+    for i in range(13):
+        bn   = model.darknet.main1[i].bn
+        conv = model.darknet.main1[i].conv
+        start = load_conv_bn(buf, start, conv, bn)
+    for i in range(1, 6):
+        bn   = model.darknet.main2[i].bn
+        conv = model.darknet.main2[i].conv
+        start = load_conv_bn(buf, start, conv, bn)
+    for i in range(2):
+        start = load_conv_bn(buf, start, model.conv1[i].conv, model.conv1[i].bn)
  
-    start = load_conv_bn (buf, start, model.conv2 [0].conv, model.conv2 [0].bn)
-    start = load_conv_bn (buf, start, model.conv  [0].conv, model.conv  [0].bn)
-    start = load_conv (buf, start, model.conv [1])
+    start = load_conv_bn(buf, start, model.conv2[0].conv, model.conv2[0].bn)
+    start = load_conv_bn(buf, start, model.conv[0].conv, model.conv[0].bn)
+    start = load_conv(buf, start, model.conv[1])
 
 
-def load_model (weights, device):
+def load_model(weights, device):
     """ load model and it's weights """
-    model = YoloV2Net ()
+    model = YoloV2Net()
     if weights:
-        load_weights (model, weights)
-        #model.load_state_dict (torch.load (weights))
+        load_weights(model, weights)
+        #model.load_state_dict(torch.load(weights))
     return model.to(device)
